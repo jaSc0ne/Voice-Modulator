02d1aea2492984a025266ca206ad5fc1a76d3e94
jaSc0ne <jason.paix@my.jcu.edu.au>
2017-06-28 11:05:19 +1000

    Added the AudioSpectra file
    Added the AudioSpectra file; which was an attempt to do real-time voice
    modulation and display the audio on a spectrum analyser and time plot.
    There is a time delay of approximately 3 seconds between when the audio
    is recorded and when the modulated audio is heard through the speakers.
    Assumed to be due to the pipeling effect in the modulation process.

---
+++ AudioSpectra.m
@@ -0,0 +1,47 @@
+
+clear;
+clc;
+
+%% Create and Initialise
+SamplesPerFrame = 1024;
+Microphone = dsp.AudioRecorder('SamplesPerFrame', SamplesPerFrame,...
+    'OutputDataType','double');
+Fs = Microphone.SampleRate;
+%Mic.DeviceName = 'Microphone (Yeti Stereo Microphone)';
+TimeScope = dsp.TimeScope('SampleRate',Fs,'TimeSpan',1,'ShowGrid',true);
+
+high_pitch = dsp.AudioPlayer('SampleRate',1.1*Fs, 'BufferSize', 4*Fs);
+higher_pitch = dsp.AudioPlayer('SampleRate', 1.2*Fs, 'BufferSize',4*Fs);
+low_pitch = dsp.AudioPlayer('SampleRate', 0.9*Fs, 'BufferSize', 4*Fs);
+lower_pitch = dsp.AudioPlayer('SampleRate', 0.8*Fs, 'BufferSize', 4*Fs);
+
+Spectra = dsp.SpectrumAnalyzer('SampleRate',Fs,'PlotAsTwoSidedSpectrum', false,...
+    'SpectralAverages',20,'FrequencyScale','Log');
+
+%% Stream processing loop
+run = true;
+while run == true
+    % Read frame from microphone
+    audioIn = step(Microphone);
+    
+    audioOut = audioIn*0.8;
+    
+    % View spectrum analysis of audio
+    step(Spectra, audioIn);
+    
+    % View audio waveform
+    step(TimeScope, [audioIn,audioOut]);
+    
+    % Play the audio
+  %  step(high_pitch,audioOut);
+    step(higher_pitch, audioOut);
+    %step(low_pitch, audioOut);
+    %step(lower_pitch, audioOut);
+   
+end
+
+%% Terminate
+release(Microphone);
+s = [];
+release(Spectra);
+s.Spectra = Spectra;
